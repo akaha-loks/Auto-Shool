919aa5445a479cdbb122b623e25a0c436a7c71a7
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2024-03-13 17:46:34 +0600

    Всякой фигни добавил
    теория, практика, главное меню

---
+++ AutoSchool/Assets/Scripts/UI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void LoadSceneId(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void LoadSceneName(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
